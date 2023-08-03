5aa2c8164ed1847d7bed51390fa517c9e6956f9b
Shota Sugo <s.sugo.pg@gmail.com>
2023-08-03 20:42:16 +0900

    init

---
+++ bin/cdk-app.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkAppStack } from "../lib/cdk-app-stack";
+
+const app = new cdk.App();
+new CdkAppStack(app, "CdkAppStack", {});
